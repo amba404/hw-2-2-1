9ce4acc686fb4a22c5d45aca38d20fda4e83e7e7
amba404 <amba.avg@gmail.com>
2024-10-22 22:46:07 +0900

    Ravenclaw implemented & some refactoring

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
                 new Gryffindor("Рон Уизли", r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101)),
         };
 
+//        testCompare(studentsG);
         for (int i = 0; i < studentsG.length; i++) {
             for (int j = 0; j < studentsG.length; j++) {
                 if (i == j) continue;
@@ -19,6 +20,7 @@ public class Main {
                 a.printCompare(b);
             }
         }
+
 
         Hufflepuff[] studentsH = {
                 new Hufflepuff("Захария Смит", r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101)),
@@ -30,6 +32,35 @@ public class Main {
             for (int j = 0; j < studentsH.length; j++) {
                 if (i == j) continue;
                 Hufflepuff a = studentsH[i], b = studentsH[j];
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+                a.printCompare(b);
+            }
+        }
+
+        Ravenclaw[] studentsR = {
+                new Ravenclaw("Чжоу Чанг", r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101)),
+                new Ravenclaw("Падма Патил", r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101)),
+                new Ravenclaw("Маркус Белби", r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101), r.nextInt(101)),
+        };
+        
+        for (int i = 0; i < studentsR.length; i++) {
+            for (int j = 0; j < studentsR.length; j++) {
+                if (i == j) continue;
+                Ravenclaw a = studentsR[i], b = studentsR[j];
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+                a.printCompare(b);
+            }
+        }
+
+    }
+
+    private static <T extends Hogwarts> void testCompare(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j) continue;
+                T a = arr[i], b = arr[j];
                 System.out.println("a = " + a);
                 System.out.println("b = " + b);
                 a.printCompare(b);
